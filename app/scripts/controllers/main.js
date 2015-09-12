98bbb0b82963370bc061e95e006f4e5dc9f5fdbc
urielaero <aero.uriel@gmail.com>
2015-09-12 00:26:19 -0500

    fix save name

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -77,12 +77,12 @@ angular.module('caminoAlExitoApp')
       );
     };*/
 
-    $scope.save = function() {
+    $scope.save = function(selectedSchool) {
      /* if(!$scope.story.signatures){
         alertNoFile();
         return;
       }*/
-
+      $scope.story.school = selectedSchool.nombre;
       $scope.saving = true;
       firebaseEntries.push().set($scope.story, function(e) {
         $scope.saving = false;
@@ -96,6 +96,57 @@ angular.module('caminoAlExitoApp')
       $anchorScroll();
     };
 
+    //update school name
+    //no founds name:
+    //["09DPR2564G", "12DEF0232L", "ES 354-85", "15EPRO723B", "26DZS0010R", undefined, "14EPR02221", "15EPR46272", "09AACOOO1M", "15DST017ID"]
 
+    /*
+    function getInfoByCCTAndUpdate(escuelas){
+      var ccts = escuelas.map(function(es){
+        return es.cct;
+      });
+      $http({
+        method: 'GET',
+        url: 'http://mte.spaceshiplabs.com/api/escuelas',
+        params: {
+          ccts: ccts.toString(),
+        }
+      }).then(function(res){
+        if(res && res.data && res.data.escuelas){
+          var founds = [];
+          res.data.escuelas.forEach(function(school, i){
+            var index = ccts.indexOf(school.cct),
+            esc = escuelas[index];
+            if(school.cct == esc.cct){
+              founds.push(esc.cct);
+              console.log("from server", school.cct, school.nombre);
+              console.log("from firebase", esc.cct);
+              console.log('index', index);
+              console.log(esc.id, 'update', school.nombre);
+              //fire.child('-JycCV2QjgzzilX3FvLk').update({name:'algo'})
+              firebaseEntries.child(esc.id).update({school: school.nombre});
+            }
+          });
+          console.log('No Found', escuelas.filter(function(es){return founds.indexOf(es.cct) == -1}).map(function(es){ return es.cct}));
+        }
+      });
+
+    }
+
+    firebaseEntries.limitToLast(100).on('value', function(snapshot){
+      var snaps = snapshot.val(),
+      escuelas = [];
+      Object.keys(snaps).forEach(function(esc, i){
+        if(snaps[esc].cct){
+          snaps[esc].id = esc;
+          escuelas.push(snaps[esc]);
+        }
+      });
+      getInfoByCCTAndUpdate(escuelas);
+
+    }, function(err){
+      console.log('err', err.code);
+    });
+    */
 
   });
