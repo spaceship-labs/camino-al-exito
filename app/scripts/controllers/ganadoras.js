4a2bb6027075d083f1020f949057a6849e1b7dac
urielaero <aero.uriel@gmail.com>
2017-08-28 19:03:50 -0500

    winners update

--- app/scripts/controllers/ganadoras.js
+++ app/scripts/controllers/ganadoras.js
@@ -8,7 +8,32 @@
  * Controller of the caminoAlExitoApp
  */
 angular.module('caminoAlExitoApp')
-  .controller('GanadorasCtrl', function () {
+  .controller('GanadorasCtrl', function ($scope, $routeParams) {
+    $scope.date = $routeParams.periodo;
+
+    $scope.schools = [
+      [
+        {cct: '14EPR0451B', name: 'Escuela Primaria Francisco Márquez', geo: 'Guadalajara, Jalisco'},
+        {cct: '10DPR0585W', name: 'Escuela Primaria Benito Juárez', geo: 'Indé, Durango'}
+      ],
+      [
+        {cct: '15EES0569S', name: 'Escuela Secundaria Ofic. No. 0439 Aquiles Serdán', geo: 'Ecatepec, Estado de México'}
+        ,{cct: '24DST0072L', name: 'Escuela Secundaria Técnica No. 65', geo: 'San Luis Potosí, San Luis Potosí'}
+      ],
+      [
+        {cct: '31EES0080K', name: 'Escuela Secundaria Elmer Orlando Gorocica Lara ', geo: 'Ucú, Yucatán'}
+        ,{cct: '31DES2016Y', name: 'Escuela Secundaria Roosevelt Ercé Barrón Pech', geo: 'Umán, Yucatán'}
+      ],
+      [
+        {cct: '04DPR0693T', name: 'Escuela Primaria Niños Héroes', geo: 'San Francisco de Campeche, Campeche'}
+        ,{cct: '26EJN0208D', name: 'Preescolar Adelina Encinas Ezre', geo: 'Hermosillo, Sonora'}
+      ],
+      [
+        {cct: '30DTV0781K', name: 'Telesecundaria Sebastián Lerdo de Tejada ', geo: 'Xalapa, Veracruz'}
+        ,{cct: '11ETV0320L', name: 'Telesecundaria No. 324', geo: 'San José de Ayala, Guanajuato'}
+      ],
+    ];
+
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
