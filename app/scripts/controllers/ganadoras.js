276eb2931efba8ed9f05e6560d370a3f79d33f1d
urielaero <aero.uriel@gmail.com>
2017-08-28 19:04:57 -0500

    jshint fix

--- app/scripts/controllers/ganadoras.js
+++ app/scripts/controllers/ganadoras.js
@@ -17,20 +17,20 @@ angular.module('caminoAlExitoApp')
         {cct: '10DPR0585W', name: 'Escuela Primaria Benito Juárez', geo: 'Indé, Durango'}
       ],
       [
-        {cct: '15EES0569S', name: 'Escuela Secundaria Ofic. No. 0439 Aquiles Serdán', geo: 'Ecatepec, Estado de México'}
-        ,{cct: '24DST0072L', name: 'Escuela Secundaria Técnica No. 65', geo: 'San Luis Potosí, San Luis Potosí'}
+        {cct: '15EES0569S', name: 'Escuela Secundaria Ofic. No. 0439 Aquiles Serdán', geo: 'Ecatepec, Estado de México'},
+        {cct: '24DST0072L', name: 'Escuela Secundaria Técnica No. 65', geo: 'San Luis Potosí, San Luis Potosí'}
       ],
       [
-        {cct: '31EES0080K', name: 'Escuela Secundaria Elmer Orlando Gorocica Lara ', geo: 'Ucú, Yucatán'}
-        ,{cct: '31DES2016Y', name: 'Escuela Secundaria Roosevelt Ercé Barrón Pech', geo: 'Umán, Yucatán'}
+        {cct: '31EES0080K', name: 'Escuela Secundaria Elmer Orlando Gorocica Lara ', geo: 'Ucú, Yucatán'},
+        {cct: '31DES2016Y', name: 'Escuela Secundaria Roosevelt Ercé Barrón Pech', geo: 'Umán, Yucatán'}
       ],
       [
-        {cct: '04DPR0693T', name: 'Escuela Primaria Niños Héroes', geo: 'San Francisco de Campeche, Campeche'}
-        ,{cct: '26EJN0208D', name: 'Preescolar Adelina Encinas Ezre', geo: 'Hermosillo, Sonora'}
+        {cct: '04DPR0693T', name: 'Escuela Primaria Niños Héroes', geo: 'San Francisco de Campeche, Campeche'},
+        {cct: '26EJN0208D', name: 'Preescolar Adelina Encinas Ezre', geo: 'Hermosillo, Sonora'}
       ],
       [
-        {cct: '30DTV0781K', name: 'Telesecundaria Sebastián Lerdo de Tejada ', geo: 'Xalapa, Veracruz'}
-        ,{cct: '11ETV0320L', name: 'Telesecundaria No. 324', geo: 'San José de Ayala, Guanajuato'}
+        {cct: '30DTV0781K', name: 'Telesecundaria Sebastián Lerdo de Tejada ', geo: 'Xalapa, Veracruz'},
+        {cct: '11ETV0320L', name: 'Telesecundaria No. 324', geo: 'San José de Ayala, Guanajuato'}
       ],
     ];
 
