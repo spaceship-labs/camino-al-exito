0a51dd519f44acc75d4a543e96e54c4b19748ee4
urielaero <aero.uriel@gmail.com>
2018-05-09 12:01:19 -0500

    winners list

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,7 +8,8 @@
  * Controller of the caminoAlExitoApp
  */
 angular.module('caminoAlExitoApp')
-  .controller('HeaderCtrl', function ($scope, $mdSidenav, login) {
+  .controller('HeaderCtrl', function ($scope) {
+    /*
     $scope.toggleLeft = function() {
       $mdSidenav('left').toggle();
     };
@@ -32,5 +33,58 @@ angular.module('caminoAlExitoApp')
     $scope.doLogin = login.showForm;
 
     $scope.logout = login.logout;
+    */
+    $scope.winners = [
+ {
+   "nombre": "Bachillerato Gral. Francisco J. Mugica",
+   "cct": "30EBH0383C",
+   "estado": "Veracruz"
+ },
+ {
+   "nombre": "CBTIS No. 62",
+   "cct": "03DCT0232V",
+   "estado": "Baja California Sur"
+ },
+ {
+   "nombre": "Preescolar Indígena Chichen-Itzá",
+   "cct": "04DCC0045J",
+   "estado": "Campeche"
+ },
+ {
+   "nombre": "Preescolar Sakpakal",
+   "cct": "04DIN0014Q",
+   "estado": "Campeche"
+ },
+ {
+   "nombre": "Primaria Comunitaria Indígena",
+   "cct": "24KPB0093V",
+   "estado": "San Luis Potosí"
+ },
+ {
+   "nombre": "Primaria Ford No 92",
+   "cct": "28DPR1452L",
+   "estado": " Tamaulipas"
+ },
+ {
+   "nombre": "Primaria José Clemente Orozco",
+   "cct": "15EPR4600T",
+   "estado": "Estado de México"
+ },
+ {
+   "nombre": "Primaria Lic. Juan Fernández Albarrán",
+   "cct": "15EPR0613W",
+   "estado": "Estado de México"
+ },
+ {
+   "nombre": "Primaria Vicente Guerrero",
+   "cct": "12DPR2372F",
+   "estado": "Guerrero"
+ },
+ {
+   "nombre": "Secundaria Técnica No. 100",
+   "cct": "14DST0100K",
+   "estado": "Jalisco"
+ }
+];
 
   });
